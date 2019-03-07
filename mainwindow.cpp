641aeabd4f6d07ffefcef239ff4d2b689d04ab11
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-03-06 23:47:37 -0600

    adding the slots for the rest of the numberic buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -20,9 +20,52 @@ void MainWindow::on_pushButtonClear_clicked()
     ui->orderDisplay->clear();
 }
 
+void MainWindow::on_pushButtonNum0_clicked()
+{
+    ui->orderDisplay->setText(ui->orderDisplay->text() + "0");
+}
 
 void MainWindow::on_pushButtonNum1_clicked()
 {
-    std::cout << "Button #1 cliecked" << std::endl;
     ui->orderDisplay->setText(ui->orderDisplay->text() + "1");
+}
+
+void MainWindow::on_pushButtonNum2_clicked()
+{
+    ui->orderDisplay->setText(ui->orderDisplay->text() + "2");
+}
+
+void MainWindow::on_pushButtonNum3_clicked()
+{
+    ui->orderDisplay->setText(ui->orderDisplay->text() + "3");
+}
+
+void MainWindow::on_pushButtonNum4_clicked()
+{
+    ui->orderDisplay->setText(ui->orderDisplay->text() + "4");
+}
+
+void MainWindow::on_pushButtonNum5_clicked()
+{
+    ui->orderDisplay->setText(ui->orderDisplay->text() + "5");
+}
+
+void MainWindow::on_pushButtonNum6_clicked()
+{
+    ui->orderDisplay->setText(ui->orderDisplay->text() + "6");
+}
+
+void MainWindow::on_pushButtonNum7_clicked()
+{
+    ui->orderDisplay->setText(ui->orderDisplay->text() + "7");
+}
+
+void MainWindow::on_pushButtonNum8_clicked()
+{
+    ui->orderDisplay->setText(ui->orderDisplay->text() + "8");
+}
+
+void MainWindow::on_pushButtonNum9_clicked()
+{
+    ui->orderDisplay->setText(ui->orderDisplay->text() + "9");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -17,7 +17,16 @@ public:
 
 private slots:
     void on_pushButtonClear_clicked();
+    void on_pushButtonNum0_clicked();
     void on_pushButtonNum1_clicked();
+    void on_pushButtonNum2_clicked();
+    void on_pushButtonNum3_clicked();
+    void on_pushButtonNum4_clicked();
+    void on_pushButtonNum5_clicked();
+    void on_pushButtonNum6_clicked();
+    void on_pushButtonNum7_clicked();
+    void on_pushButtonNum8_clicked();
+    void on_pushButtonNum9_clicked();
 
 private:
     Ui::MainWindow *ui;
