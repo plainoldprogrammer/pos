e1a358653265a4f2f57e35cdb08640707e813500
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-03-19 00:13:26 -0600

    implementing the ticket class to hold in memory the ticket

--- ticket.cpp
+++ ticket.cpp
@@ -2,5 +2,48 @@
 
 ticket::ticket()
 {
+    header = "";
+    body = "";
+    footer = "";
+    ticketTotalAmount;
+}
 
+void ticket::setHeader(QString newHeader)
+{
+    header = newHeader;
+}
+
+void ticket::setBody(QString newBody)
+{
+    body = newBody;
+}
+
+void ticket::setFooter(QString newFooter)
+{
+    footer = newFooter;
+}
+
+void ticket::setTicketTotalAmount(int newTicketTotalAmount)
+{
+    ticketTotalAmount = newTicketTotalAmount;
+}
+
+QString ticket::getHeader()
+{
+    return header;
+}
+
+QString ticket::getBody()
+{
+    return body;
+}
+
+QString ticket::getFooter()
+{
+    return footer;
+}
+
+int ticket::getTicketTotalAmount()
+{
+    return ticketTotalAmount;
 }
--- ticket.h
+++ ticket.h
@@ -1,12 +1,26 @@
 #ifndef TICKET_H
 #define TICKET_H
 
+#include <QString>
 
 class ticket
 {
 public:
     ticket();
 private:
+    QString header;
+    QString body;
+    QString footer;
+    int ticketTotalAmount;
+
+    void setHeader(QString);
+    void setBody(QString);
+    void setFooter(QString);
+    void setTicketTotalAmount(int);
+    QString getHeader();
+    QString getBody();
+    QString getFooter();
+    int getTicketTotalAmount();
 };
 
 #endif // TICKET_H
