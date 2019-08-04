3308a5f9e887cbc4cebecb8da0dcd7936c76c273
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-08-04 18:12:38 -0500

    adding deleted files to get working the ui

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setStyleSheet( "QMainWindow { background-color: #808080 }" );
+    w.show();
+
+    return a.exec();
+}
