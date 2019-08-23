748b9f32dc7354c0eafe8c7de2863f15df66a836
plainoldprogrammer <plainoldprogrammer@gmail.com>
2019-08-22 21:14:20 -0500

    resizing automatically the columns in the tickets table

--- TicketsTableWindow.cpp
+++ TicketsTableWindow.cpp
@@ -25,4 +25,8 @@ void TicketsTableWindow::resizeEvent(QResizeEvent *event)
 {
     qDebug() << "Resizing the table window";
     ui->tableView->resize(this->size().width() - 20, this->size().height() - 20);
+    ui->tableView->setColumnWidth(0, (this->width() / 4) - 10);
+    ui->tableView->setColumnWidth(1, (this->width() / 4) - 10);
+    ui->tableView->setColumnWidth(2, (this->width() / 4) - 10);
+    ui->tableView->setColumnWidth(3, (this->width() / 4) - 10);
 }
